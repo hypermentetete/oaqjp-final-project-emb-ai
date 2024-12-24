98683578ae6ad14d708b874b151f234f580fb1c5
hypermente <hypermente@hotmail.com>
2024-12-24 10:08:22 -0500

    Flask publish

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route('/emotionDetector')
+def call_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    r = emotion_detector(text_to_analyze)
+    return f"For the given statement, the system response is 'anger': {r['anger']}, " \
+            f"'disgust': {r['disgust']}, " \
+            f"'fear': {r['fear']}, " \
+            f"'joy': {r['joy']}, " \
+            f"'sadness': {r['sadness']}. " \
+            f"The dominant emotion is {r['dominant_emotion']}. "
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
